c220d63a9396b84c2ea22ed864853c293abab273
nvcengineer@gmail.com <nvcengineer@gmail.com>
2023-02-22 09:17:08 +0700

    router va MVC

--- src/server.js
+++ src/server.js
@@ -1,16 +1,19 @@
 
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
+import initWebRoute from './route/web';
+
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+// setup view engine
 configViewEngine(app);
 
-app.get('/', (req, res) => {
-    res.render('index.ejs');
-})
+// init web route
+initWebRoute(app);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
