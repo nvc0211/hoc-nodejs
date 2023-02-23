310e4893ec77e22ac2dcc40059397fa5ded68999
NGUYEN VAN CUONG <nvcengineer@gmail.com>
2023-02-23 11:07:09 +0700

    Insert newuer to Database

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -16,7 +16,17 @@ let getDetailPage = async (req, res) => {
     return res.send(JSON.stringify(user[0]))
 }
 
+let createNewUser = async (req, res) => {
+    console.log('check req:', req.body)
+    let { firstName, lastName, email, address } = req.body;
+    await pool.execute('insert into users(firstName,lastName, email,address) values (?,?,?,?)',
+        [firstName, lastName, email, address])
+    // return res.send('Call post create new user')
+    return res.redirect('/'); // Auto return Homepage
+}
 module.exports = {
     getHomepage,
-    getDetailPage
+    getDetailPage,
+    createNewUser
+
 }
